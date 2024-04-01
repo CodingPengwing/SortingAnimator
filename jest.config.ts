3b452af03b0fb11c8a40a5a19b600fd1cc339261
Tony Dang <tungtonydang@gmail.com>
2024-04-01 18:29:07 +1100

    Add babel config for testing

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,12 @@ import { defaults } from "jest-config";
 
 const config: Config = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, "mts"],
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  testMatch: ["**/*.test.(ts|tsx|mts)"],
 };
 
 export default config;
